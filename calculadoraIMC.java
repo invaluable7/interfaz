ed2429b849697725eb6711205e2c6fa48e6e1f6f
inventario_tienda-POO <independencia0725@gmail.com>
2025-09-01 17:24:39 -0500

    front

---
+++ calculadoraIMC.java
@@ -0,0 +1,18 @@
+        package interfaz;
+import interfaz.calculadora;
+
+import javax.swing.*;
+
+        public class calculadoraIMC {
+    public static void main(String[] args) {
+        calculadora n = new calculadora();
+        n.setVisible(true);
+        n.setLocationRelativeTo(null);
+        n.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        
+     
+    }
+            
+}
